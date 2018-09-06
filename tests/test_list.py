95f7cada2d121b9bac5c0cdd607a70f2796e9d07
3Nakajugo <edinahmargarita@gmail.com>
2018-09-06 14:11:25 +0300

    create file missing_value.py sort_list.py and folder tests

---
+++ tests/test_list.py
@@ -0,0 +1,17 @@
+import unittest
+
+from missing_value import find_number
+
+
+class test_sort_list(unittest.TestCase):
+    def test_find_missing(self):
+        self.assertEqual(find_number([1,2,3,5,6,7,9]),[4,8])
+
+    def test_list(self):
+        self.assertRaises(TypeError, find_number,'123')
+
+
+
+
+
+    
